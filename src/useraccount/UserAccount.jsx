c08f4c3ad9a8f01064085e628ace341bda11a8a0
Pakapool Boonma <dinodragoz77@gmail.com>
2024-03-26 13:26:59 +0800

    Added responsive navbar

--- src/useraccount/UserAccount.jsx
+++ src/useraccount/UserAccount.jsx
@@ -4,8 +4,59 @@ import './UserAccount.css'
 import UserIcon from "../assets/user-circle.svg";
 import LogoutIcon from '@mui/icons-material/Logout';
 
+
+
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
+const drawerWidth = 240;
+const navItems = ['Search Courses', 'Schedule'];
+
 function UserAccount({ setIsAuthenticated, setCourses, name }) {
     const navigate = useNavigate();
+
+    const { window } = PropTypes.func;
+    const [mobileOpen, setMobileOpen] = React.useState(false);
+
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
+
+    const drawer = (
+        <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <Typography variant="h6" sx={{ my: 2 }}>
+                NTHU-Courses
+            </Typography>
+            <Typography variant="h7" sx={{ my: 2 }}>
+                {name}
+            </Typography>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }}>
+                            <ListItemText primary={item} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
+
+    const container = window !== undefined ? () => window().document.body : undefined;
 
     const logout = () => {
         localStorage.removeItem('userToken');
@@ -19,15 +70,64 @@ function UserAccount({ setIsAuthenticated, setCourses, name }) {
     };
 
     return (
-        <div className='user-account text-black'>
-            <img src={UserIcon} alt="user icon" className="user-icon" />
-            <h2 className="username-text">{name}</h2>
-            <div className='sign-out-button'>
-                <button onClick={handleLogout}>
-                    <LogoutIcon />
-                </button>
-            </div>
-        </div>
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar component="nav">
+                <Toolbar
+                    sx={{ minHeight: 64 }}
+                >
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography
+                        variant="h6"
+                        component="div"
+                        sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+                    >
+                        NTHU-Courses
+                    </Typography>
+                    <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+                        {navItems.map((item) => (
+                            <Button key={item} sx={{ color: '#fff' }}>
+                                {item}
+                            </Button>
+                        ))}
+                        <Button sx={{ color: '#fff' }}>{name}</Button>
+                        <Button
+                            sx={{ color: '#fff', minWidth: 20 }}
+                            onClick={handleLogout}
+                        >
+                            <LogoutIcon className='sign-out-button' />
+                        </Button>
+
+                    </Box>
+                </Toolbar>
+            </AppBar>
+            <nav>
+                <Drawer
+                    container={container}
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
+        </Box>
+
     );
 }
 
